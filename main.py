cb1478bb6b790f9540fe42eb62f4c65bcc861a07
Fairson <fs00602@surrey.ac.uk>
2023-09-21 21:42:39 +0100

    configurations

--- main.py
+++ main.py
@@ -37,6 +37,19 @@ for repo_dict in repo_dicts:
     
     # Make visualization
     my_style = LS('#333366', base_style=LCS)
+    my_style.title_font_size = 24
+    my_style.label_font_size = 14
+    my_style.major_label_font_size = 18
+    
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+    
+    
+    
     chart = pygal.Bar(style=my_style, x_label_rotating=45, show_legend=False)
     chart.title = 'Most-Starred Python Projects on Github'
     chart.x_labels = names
@@ -44,6 +57,8 @@ for repo_dict in repo_dicts:
     
     chart.add('', stars)
     chart.render_to_file('python_repos.svg')
+    
+    
     
     
     
